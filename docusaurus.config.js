aa194707055e449bfd4c6f6d5da87bcaa6a5a899
Itay Elgazar <Itayekk1@gmail.com>
2023-05-28 16:47:03 +0200

    fix: build

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://docs.pezzo.ai',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs',
+  baseUrl: '/pezzo',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
